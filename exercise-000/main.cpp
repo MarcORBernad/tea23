18cdfee706beb406305e25ef66f40d7ed2b1c382
MarcORBernad <marcbernad@icloud.com>
2025-10-10 11:52:46 +0200

    part 1

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -4,22 +4,22 @@
 #include "config.h"
 
 int main(int argc, char **argv) {
-
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    //fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
+    //fmt::print("Hello, {} in version {}!\n", tea::PROJECT_NAME, tea::PROJECT_VER);
+    // Part 0
     fmt::print("Anzahl Argumente (argc): {}\n", argc);
 
     for (size_t i = 0; i < argc; i++)
     {
         fmt::print("argv[{}]: {} (Adresse: {})\n", i, argv[i], fmt::ptr(&argv[i]));
     }
-    
 
-    /* INSERT YOUR CODE HERE */
+    // Part 1
+    int x = 10;
+    int *p = &x;
+    *p = 42;
+
+    fmt::print("x: {}, Adresse: {}\n", x, fmt::ptr(&x));
+    fmt::print("p: {}, Adresse: {}\n", *p, fmt::ptr(p));
 
     return 0; /* exit gracefully*/
 }
